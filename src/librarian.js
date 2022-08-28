618d0dcf3c961bc41378d4b8bf1cbab83427021c
Dana Truong <tramtramphotos@gmail.com>
2022-08-28 13:36:45 -0500

    Update librarian file

--- src/librarian.js
+++ src/librarian.js
@@ -14,8 +14,14 @@ class Librarian {
     this.findBook = function(bookTitle) {
       var allBooks = this.library.shelves.fantasy.concat(this.library.shelves.fiction, this.library.shelves.nonFiction)
       for (var i = 0; i < allBooks.length; i++) {
-        if (allBooks[i].title === bookTitle) {
+        if (allBooks[i].title === bookTitle && allBooks[i].genre === 'fantasy') {
           this.library.shelves.fantasy.splice(i, 1)
+          return `Yes, we have ${bookTitle}`
+        } else if (allBooks[i].title === bookTitle && allBooks[i].genre === 'fiction') {
+          this.library.shelves.fiction.splice(i, 1)
+          return `Yes, we have ${bookTitle}`
+        } else if (allBooks[i].title === bookTitle && allBooks[i].genre === 'nonFiction') {
+          this.library.shelves.nonFiction.splice(i, 1)
           return `Yes, we have ${bookTitle}`
         }
         return `Sorry, we do not have ${bookTitle}`
